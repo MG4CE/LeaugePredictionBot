47487fb03762fb525149be6eac95191f444690b0
MG4CE <maged.othm@gmail.com>
2023-07-12 00:17:11 -0400

    added Tuple typing to is_allowed_game_type

--- api/league/league_utils.py
+++ api/league/league_utils.py
@@ -1,10 +1,11 @@
+from typing import Tuple
 
 def get_champion_name(champions: dict, champ_id: int) -> str:
     for champ in champions:
         if int(champions[champ]['key']) == champ_id:
             return champ
 
-def is_allowed_game_type(game_type: str, game_mode: str):
+def is_allowed_game_type(game_type: str, game_mode: str) -> Tuple[bool, str]:
     if game_type == "MATCHED_GAME":
         if game_mode == "CLASSIC":
             return (True, "Summoner's Rift")
